accfa34b212c0863dc590c412dd0422e1c81ab4a
Bruno Lima <brunsalve1@hotmail.com>
2015-01-07 18:09:49 -0200

    consertando erros

--- public/functions/funcoes.php
+++ public/functions/funcoes.php
@@ -19,7 +19,7 @@
     
     
         if  ($urllimpa==""){  
-            require_once('/src/home.php');
+            require_once('../src/home.php');
         }
     
         elseif  (array_key_exists($path, $rotasValidas)){
