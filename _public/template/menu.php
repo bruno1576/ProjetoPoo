1225f9adfddaa2864c05e00a23d8ffe4b532d2e9
Bruno Lima <brunsalve1@hotmail.com>
2015-01-07 17:21:20 -0200

    deletando pastas

--- _public/template/menu.php
+++
@@ -1,8 +0,0 @@
- <h2> Menu </h2> 
-<ul class="nav nav-tabs nav-stacked">
-     <li> <a href="home">
- <i class="icon-star"></i> Pagina inicial </a> </li>
-     <li> <a href="ListaClientes"> <i class="icon-star">
- </i> Listagem de clientes </a> </li> 
- 
-     </ul> </div> <!-- COLUNA OCUPANDO 10 ESPAÇOS NO GRID --> 
