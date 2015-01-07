f0ba35a356a7f00af45563b4c58f86b7309de00d
Bruno Lima <brunsalve1@hotmail.com>
2015-01-06 23:14:15 -0200

    Projeto POO Fase 1

---
+++ _public/template/menu.php
@@ -0,0 +1,8 @@
+ <h2> Menu </h2> 
+<ul class="nav nav-tabs nav-stacked">
+     <li> <a href="home">
+ <i class="icon-star"></i> Pagina inicial </a> </li>
+     <li> <a href="ListaClientes"> <i class="icon-star">
+ </i> Listagem de clientes </a> </li> 
+ 
+     </ul> </div> <!-- COLUNA OCUPANDO 10 ESPAÇOS NO GRID --> 
---
+++ _public/template/topo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML> <html lang="en-US"> <head> <meta charset="UTF-8">
+ <title>Twitter Bootstrap</title> <!-- JQUERY --> 
+ <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" /> <!-- TWITTER BOOTSTRAP JS --> 
+ </head>
+ <body> <!-- HEADER --> 
+ <header class="container-fluid"> 
+ <div class="row-fluid"> 
+ <div class="span12"> 
+ <div class="navbar">
+ <div class="navbar-inner">
+ <div class="container">
+ <h2>Projeto Fase 1</h2> 
+ </div>
+ </div>
+ </div>
+ </div> </div>
+ </header> <!-- / HEADER --> <!-- CLASSE QUE DEFINE O CONTAINER COMO FLUIDO (100%) -->
+ <!-- COLUNA OCUPANDO 2 ESPAÇOS NO GRID --> <div class="span2"> 
